7fa0620803728007a858737a9c97e758f6af5945
Destoky <adestoky@gmail.com>
2020-12-25 23:43:33 +0100

    6- Hydratation (3méthodes)
    -fonction hydrater

--- projet/classes/Etudiant.php
+++ projet/classes/Etudiant.php
@@ -4,14 +4,18 @@
 class Etudiant
 {
   private $_prenom;
+  private $_nom;
   const ECOLE = "Ieps Fléron";
 
   private static $_nombreObjetsCrees = 0;
 
   //CONSTRUCTEUR 
-  public function __construct()
+  public function __construct(array $data = null)
   {
     SELF::$_nombreObjetsCrees++;
+    if ($data) :
+      $this->hydrater($data);
+    endif;
   }
 
   //DESTRUCTEUR 
@@ -33,11 +37,37 @@ class Etudiant
     return $this->_prenom;
   }
 
+  public function getNom()
+  {
+    return $this->_nom;
+  }
+
   //SETTERS
   public function setPrenom(string $data = null)
   {
     if ($data) :
       $this->_prenom = $data;
+    endif;
+  }
+
+  public function setNom(string $data = null)
+  {
+    if ($data) :
+      $this->_nom = $data;
+    endif;
+  }
+
+  //AUTRES METHODES
+  public function hydrater(array $data = null)
+  {
+    if ($data) :
+      foreach ($data as $propriete => $valeur) {
+        $nomMethode = "set" . ucfirst($propriete);
+
+        if (method_exists($this, $nomMethode)) :
+          $this->$nomMethode($valeur);
+        endif;
+      }
     endif;
   }
 }
--- projet/index.php
+++ projet/index.php
@@ -2,9 +2,43 @@
 
 require_once "noyau/init.php";
 
+//----------Méthode 1 "Classique"----------
 
+//Instanciation d'un objet
 $etudiant1 = new Etudiant();
-$etudiant1->setPrenom("Pascal");
 
+//Hydratation manuelle d'une propriétés
+$etudiant1->setPrenom("Pascal");
+$etudiant1->setNom("Lacroix");
+
+
+//----------Méthode 2 "fct hydrater dans Etudiant.php----------
+
+//Instanciation d'un objet
+$etudiant2 = new Etudiant();
+
+//Hydratation avec fonction
+$etudiant2->hydrater([
+  "prenom" => "Rémy",
+  "nom" =>"Dupont"
+]);
+
+
+//----------Méthode 3 "lancement fct hydrater dans le construct----------
+
+//Instanciation d'un objet et hydration en 1 seul étape
+$etudiant3 = new Etudiant([
+  "prenom" => "Julien",
+  "nom" =>"Costenaro"
+]);
+
+
+
+//Utilisation de l'objet
 echo $etudiant1->getPrenom();
+echo "<br>";
+echo $etudiant2->getPrenom();
+echo "<br>";
+echo $etudiant3->getPrenom();
+echo "<br>";
 
